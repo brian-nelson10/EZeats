fd1e616b4c95f0380acd3d9164d626f042b6b55a
brian nelson <bn3l10@gmail.com>
2023-09-06 00:32:13 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { AnimatePresence } from 'framer-motion';
 import ReactDOM from 'react-dom/client';
 import './input.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AnimatePresence>
+    <BrowserRouter>
     <App />
-    </AnimatePresence>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
