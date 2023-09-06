3d82388b2086a18a3041b5a1b36357f74a4b352d
brian nelson <bn3l10@gmail.com>
2023-09-05 22:45:32 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AnimatedRoutes from './components/AnimatedRoutes';
-import ScrollTop from './components/ScrollTop';
+// import ScrollTop from './components/ScrollTop';
 
 function App() {
   
   return (
     <>   
       <BrowserRouter basename={process.env.PUBLIC_URL} >
-      <ScrollTop />
+      {/* <ScrollTop /> */}
       <AnimatedRoutes />
     </BrowserRouter>
     </>
--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route  } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Home from '../pages/Home';
 import Contact from '../pages/Contact';
@@ -8,37 +8,37 @@ import FoodTruck from "../pages/FoodTruck";
 import NotFound from "../pages/NotFound";
 
 function AnimatedRoutes() {
-    
+
     return (
         <AnimatePresence mode="wait">
-           
-<Routes >
-            <Route
-               path="/"
-              element={<Home/>}
-              />
-              <Route
-              path="/home"
-              element={<Home/>}
-              />
-              <Route
-              path="/contact"
-              element={<Contact />}
-              />
-              <Route
-              path="/catering"
-              element={<Catering/>}
-              />
-              <Route
-              path="/foodtruck"
-              element={<FoodTruck/>}
-              />
-              <Route
-              path="*"
-              element={<NotFound/>}
-              />
-          </Routes>
-          </AnimatePresence>
+
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/home"
+                    element={<Home />}
+                />
+                <Route
+                   exact path="/contact"
+                    element={<Contact />}
+                />
+                <Route
+                    path="/catering"
+                    element={<Catering />}
+                />
+                <Route
+                    path="/foodtruck"
+                    element={<FoodTruck />}
+                />
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+            </Routes>
+        </AnimatePresence>
     );
 };
 export default AnimatedRoutes;
